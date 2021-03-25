5fa95523abf6df4c2da4857ee7bc275eb2c177e0
drAbreu <44996651+drAbreu@users.noreply.github.com>
2021-03-25 11:08:55 +0100

    add rshiny instructions

--- install-r-studio-shiny.sh
+++ install-r-studio-shiny.sh
@@ -1,18 +1,28 @@
 #install R
 sudo amazon-linux-extras install R4
 #install RStudio-Server 
-# If this would not worl, make sure to visit https://rstudio.com/products/rstudio/download-server/redhat-centos/ for the latest versions of R studio
+# If this would not work, make sure to visit https://rstudio.com/products/rstudio/download-server/redhat-centos/ for the latest versions of R studio
 wget https://download2.rstudio.org/server/centos7/x86_64/rstudio-server-rhel-1.4.1106-x86_64.rpm
 sudo yum install -y --nogpgcheck rstudio-server-rhel-1.4.1106-x86_64.rpm
 
-useradd rstudio 
+sudo useradd rstudio 
 # add your favourite password in here. For the demo, we will use rstudio
-echo rstudio:rstudio | chpasswd
+# if the line behind does not work try
+# sudo passwd rstudio
+sudo echo rstudio:rstudio | chpasswd
 
 # remove the conf file, cant be used in free Rstudio version
 sudo rm -rf /etc/rstudio/rserver.conf 
 sudo rstudio-server stop
 sudo rstudio-server start
+
+#the instructions to install Rshiny can be found here https://rstudio.com/products/shiny/download-server/redhat-centos/
+sudo su - \
+-c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
+
+wget https://download3.rstudio.org/centos7/x86_64/shiny-server-1.5.16.958-x86_64.rpm
+sudo yum install --nogpgcheck shiny-server-1.5.16.958-x86_64.rpm
+
 #configure java, choose 1 as your selection option for java 8 configuration 
 sudo /usr/sbin/alternatives --config java 
 #run command below to add Java support to R 
